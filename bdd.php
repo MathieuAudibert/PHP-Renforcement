2a3fa24d940d39fd97d9195ce7dcbb5ad378b744
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-09-09 16:03:59 +0200

    bdd essai fix

--- bdd.php
+++ bdd.php
@@ -2,13 +2,12 @@
 require_once('./cred.env');
 
 $host = $_ENV['DB_HOST'];
-$port = 5432;
 $dbname = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];
 
 try {
-    $bdd = new PDO("pgsql:host={$host};port=5432;dbname={$dbname}, {$user}, {$password}");
+    $bdd = new PDO("pgsql:host={$host};port=5432;dbname={$dbname},{$user},{$password}");
 } catch (PDOException $e) {
     echo $e->getMessage();
     echo "Pas connecté ";
