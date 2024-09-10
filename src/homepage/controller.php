8ea0721807ef475ecaa37acc3e0e7f1ca07694a9
Mathieu Audibert <mathieu.audibert27@gmail.com>
2024-09-10 16:20:23 +0200

    controller et model pas a jour

--- src/homepage/controller.php
+++ src/homepage/controller.php
@@ -11,3 +11,17 @@ function controller_homepage()
         echo "Une erreur s'est produite : " . $e->getMessage();
     }
 }
+
+class MusiqueController {
+
+    private $model;
+
+    public function __construct() {
+        $this->model = new MusiqueModel();
+    }
+
+    public function afficherMusiques() {
+        $musiques = $this->model->getAllMusique();
+        require __DIR__ . '/view.php'; 
+    }
+}
--- src/homepage/model.php
+++ src/homepage/model.php
@@ -1,2 +1,19 @@
 <?php
-function model_homepage() {}
+require_once ('../utils/bdd.php');
+
+class MusiqueModel {
+
+    private $db;
+
+    public function __construct() {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function getAllMusique() {
+        $query = $this->db->prepare("SELECT * FROM musiques");
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+?>
