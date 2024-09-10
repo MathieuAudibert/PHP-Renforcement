d10f86da3116b85960763689a4dfb260a317bb26
Mathieu Audibert <mathieu.audibert27@gmail.com>
2024-09-10 10:41:51 +0200

    ptits modifs arborescence + creation classe musique

--- src/register/model.php
+++ src/register/model.php
@@ -1,6 +1,43 @@
 <?php
-function model_register()
-{
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+
+declare(strict_types=1);
+
+abstract class Formulaire {
+    abstract public function creerChamp(): Champ;
+
+    public function afficherFormulaire() {
+        $champ = $this->creerChamp();
+        return "Formulaire avec : " . $champ->afficher();
+    }
 }
+
+class FormulaireTexte extends Formulaire {
+    public function creerChamp(): Champ {
+        return new ChampTexte();
+    }
+}
+
+class FormulaireMotDePasse extends Formulaire {
+    public function creerChamp(): Champ {
+        return new ChampMotDePasse();
+    }
+}
+
+interface Champ {
+    public function afficher(): string;
+}
+
+class ChampTexte implements Champ {
+    public function afficher(): string {
+        return "Champ de texte";
+    }
+}
+
+class ChampMotDePasse implements Champ {
+    public function afficher(): string {
+        return "Champ de mot de passe";
+    }
+}
+
+$formulaire = new FormulaireTexte();
+echo $formulaire->afficherFormulaire(); 
--- utils/musique.php
+++ utils/musique.php
@@ -2,7 +2,86 @@
 
 declare(strict_types=1);
 
-class Musique {
-    
+abstract class Musique {
+    protected $titre;
+    protected $artiste;
+    protected $album;
+    protected $duree; 
+    protected $genre;
 
+    public function __construct($titre, $artiste, $album, $duree, $genre) {
+        $this->titre = $titre;
+        $this->artiste = $artiste;
+        $this->album = $album;
+        $this->duree = $duree;
+        $this->genre = $genre;
+    }
+
+    public function getTitre() {
+        return $this->titre;
+    }
+
+    public function getArtiste() {
+        return $this->artiste;
+    }
+
+    public function getAlbum() {
+        return $this->album;
+    }
+
+    public function getDuree() {
+        return $this->duree;
+    }
+
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    public function setTitre($titre) {
+        $this->titre = $titre;
+    }
+
+    public function setArtiste($artiste) {
+        $this->artiste = $artiste;
+    }
+
+    public function setAlbum($album) {
+        $this->album = $album;
+    }
+
+    public function setDuree($duree) {
+        $this->duree = $duree;
+    }
+
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    abstract public function streamer();
 }
+
+class MusiqueFabrique {
+    public static function createMusique($genre, $titre, $artiste, $album, $duree, $param1) {
+        switch (strtolower($genre)) {
+            case 'rap':
+                return new RapConcrete($titre, $artiste, $album, $duree, $param1);
+                
+            case 'jazz':
+                return new JazzConcrete($titre, $artiste, $album, $duree, $param1);
+                
+            case 'classique':
+                return new ClassiqueConcrete($titre, $artiste, $album, $duree, $param1);
+                
+            default:
+                throw new InvalidArgumentException("Genre de musique pas existe.");
+        }
+    }
+}
+
+?>
+
+
+
+
+
+?>
