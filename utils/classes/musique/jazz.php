f64abd21a2715959f0a6f6d6c49a08cb35e455f5
Mathieu Audibert <mathieu.audibert27@gmail.com>
2024-09-10 11:03:41 +0200

    creation de jazz

---
+++ utils/classes/musique/jazz.php
@@ -0,0 +1,21 @@
+<?php 
+
+declare(strict_types=1);
+
+require_once 'musique.php';
+
+class Jazz extends Musique {
+    public function __construct($titre, $artiste, $album, $duree, $niveau_acces) {
+        parent::__construct($titre, $artiste, $album, $duree, 'Jazz', $niveau_acces);
+    }
+
+    public function streamer($niveau_abonnement) {
+        if ($niveau_abonnement >= $this->niveau_acces) {
+            return "Lecture de '{$this->getTitre()}' par {$this->getArtiste()}.";
+        } else {
+            return "Vous n'avez pas le bon abonnement.";
+        }
+    }
+}
+
+?>
